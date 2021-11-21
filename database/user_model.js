f5ee528695ad7e84c9320686c94d15deea01d985
Joko <edo@bfi.io>
2021-11-21 17:21:54 +0700

    Finish backend create account

---
+++ database/user_model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	phoneNumber: {
+		type: String,
+		required: true,
+	},
+	businessName: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("User", userSchema);
--- routes/routes.js
+++ routes/routes.js
@@ -5,13 +5,81 @@ const router = express.Router();
 // path
 const path = require("path");
 
+// authentication
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// database
+const User = require(path.join(__dirname, "..", "database", "user_model"));
+
 // welcome page
 router.get("/", (req, res) => {
-	res.render("signin");
+	res.render("homepage");
+});
+
+router.get("/signin", (req, res) => {
+	const message = "";
+
+	res.render("signin", { message });
+});
+
+router.post("/signin", async (req, res) => {
+
+	res.redirect("/signin");
 });
 
 router.get("/signup", (req, res) => {
-	res.render("signup");
+	const message = "";
+
+	res.render("signup", { message });
+});
+
+router.post("/signup", async (req, res) => {
+	try {
+		const email = req.body.email.toLowerCase();
+		const { phoneNumber } = req.body;
+		const { businessName } = req.body;
+		const { password } = req.body;
+
+		const user = await User.findOne({ email });
+
+		// email already used
+		if (user) {
+			res.redirect("/signup/email-used");
+		}
+		else {
+			const hashedPassword = await bcrypt.hash(password, 10);
+
+			User.create({
+				email,
+				phoneNumber,
+				businessName,
+				password: hashedPassword,
+			});
+
+			res.redirect("/signup/success");
+		}
+	} catch (err) {
+		console.log(err);
+		res.redirect("/signup/error");
+	}
+});
+
+router.get("/signup/:status", (req, res) => {
+	const status = req.params.status;
+
+	let message = "";
+	if (status == "error") {
+		message = "There's an error";
+	}
+	else if (status == "success") {
+		message = "Success creating account.";
+	}
+	else if (status == "email-used") {
+		message = "Email already used.";
+	}
+
+	res.render("signup", { message });
 });
 
 // auth middleware
